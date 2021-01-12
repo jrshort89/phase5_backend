c7393943f867e0feb91071e2e703535568f4211a
jrshort89 <jrshort89@gmail.com>
2021-01-12 16:24:06 -0500

    controller setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_211626) do
+ActiveRecord::Schema.define(version: 2021_01_12_212159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2021_01_12_211626) do
   create_table "solutions", force: :cascade do |t|
     t.string "text"
     t.integer "lesson_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
