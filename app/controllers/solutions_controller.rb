144aaa2aa2462cb0424043c470aec946621369b1
jrshort89 <jrshort89@gmail.com>
2021-01-22 10:59:05 -0500

    solution post

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,2 +1,15 @@
 class SolutionsController < ApplicationController
+    def create
+        @solution = Solution.new solution_params
+        if @solution.save
+            UserSolution.create user_id: params[:user_id], solution_id: @solution.id
+            return render json: @solution
+        end
+    end
+
+    private
+
+    def solution_params
+        params.require(:solution).permit(:text, :lesson_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
       get '/lessons/:id', to: 'lessons#show'
       post '/lessons', to: 'lessons#create'
 
+      post '/solutions',to: 'solutions#create'
+
       get '/subjectlessons', to: 'subject_lessons#index'
       get '/subjects', to: 'subjects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
