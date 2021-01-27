62da2191d6b03e786bec6a9e4f7fa6bf25ce4fe8
jrshort89 <jrshort89@gmail.com>
2021-01-27 11:27:53 -0500

    migrate solutions user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_185726) do
+ActiveRecord::Schema.define(version: 2021_01_27_162618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_185726) do
     t.integer "lesson_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "subject_lessons", force: :cascade do |t|
