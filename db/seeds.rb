7e659de390bb87c217b42f0d90cc743ab37b896d
jrshort89 <jrshort89@gmail.com>
2021-01-31 21:00:18 -0500

    db update

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,13 @@ Solution.destroy_all
 SubjectLesson.destroy_all
 UserSolution.destroy_all
 
-# 5.times {Lesson.create(text: Faker::Lorem.sentence, subject: Faker::Lorem.sentence)}
+5.times {Lesson.create(text: Faker::Lorem.sentence, subject: Faker::Lorem.sentence)}
 
-# 5.times {Subject.create(name: Faker::Lorem.sentence(word_count: 2))}
+5.times {Subject.create(name: Faker::Lorem.sentence(word_count: 2))}
 
-# 5.times {SubjectLesson.create(subject_id: Subject.all.sample.id, lesson_id: Lesson.all.sample.id, name: Faker::Hacker.abbreviation, number: rand(1..6))}
+5.times {SubjectLesson.create(subject_id: Subject.all.sample.id, lesson_id: Lesson.all.sample.id, name: Faker::Hacker.abbreviation, number: rand(1..6))}
 
-# 5.times {Solution.create(text: Faker::Lorem.sentence)}
+5.times {Solution.create(text: Faker::Lorem.sentence)}
 
-# 5.times {UserSolution.create(user_id: User.first.id, solution_id: Solution.all.sample.id)}
+5.times {UserSolution.create(user_id: User.first.id, solution_id: Solution.all.sample.id)}
 
