16a9da398eee3d54c1e42e516f394325821accdd
jrshort89 <jrshort89@gmail.com>
2021-01-31 21:23:20 -0500

    cors fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,6 +50,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.allowed_cors_origins = "*"
-
 end
